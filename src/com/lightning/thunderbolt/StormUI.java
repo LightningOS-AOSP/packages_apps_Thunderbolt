819553af60d23a6df838a3c773e3f0206222df8e
MODEDGES <ofwargane30@gmail.com>
2026-07-28 15:18:46 +0100

    Thunderbolt: Add PreferenceController

--- src/com/lightning/thunderbolt/StormUI.java
+++ src/com/lightning/thunderbolt/StormUI.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.droidx.customize;
+package com.lightning.thunderbolt;
 
 import android.app.Activity;
 import android.content.ContentResolver;
